ce4a0423bad6d9925d222cc26ead00732976b065
Balazs Faludi <balazsfaludi@gmail.com>
2014-03-17 20:18:23 +0100

    Initial Commit

---
+++ BFStretchView/BFStretchView.h
@@ -0,0 +1,13 @@
+//
+//  BFStretchView.h
+//  BFStretchView
+//
+//  Created by Balázs Faludi on 17.03.14.
+//  Copyright (c) 2014 Balazs Faludi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFStretchView : NSObject
+
+@end
---
+++ BFStretchView/BFStretchView.m
@@ -0,0 +1,13 @@
+//
+//  BFStretchView.m
+//  BFStretchView
+//
+//  Created by Balázs Faludi on 17.03.14.
+//  Copyright (c) 2014 Balazs Faludi. All rights reserved.
+//
+
+#import "BFStretchView.h"
+
+@implementation BFStretchView
+
+@end
